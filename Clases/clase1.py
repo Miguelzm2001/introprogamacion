bde68a7b191e2a46f442eebdb525031b61bd91c3
Miguelzm2001 <miguezuletamedina@gmail.com>
2021-02-15 17:54:03 -0500

    Taller1

--- Clases/clase1.py
+++ Clases/clase1.py
@@ -1,7 +1,8 @@
 print("Hola Mndo")
-
+#Lo que está dentro de las comillas es una frase que el sistema retiene 
 Saludo = "hola mundo"
 Edad = 19
 Estatura = 1.80
 
 print(Saludo, Edad, Estatura)
+#para poder imprimir debo poner print
--- Clases/clase2.py
+++ Clases/clase2.py
@@ -4,13 +4,16 @@ print (pruebaV)
 print (pruebaF)
 pruebaV = pruebaF
 print (pruebaV)
+#Para poner falso y verdadero necesito especificar los codigos anteriores
 edad = 27
 estatura = 1.67
 peso = 84
 NOMBRE = "Miguel Zuleta"
 print ("#"*15, "Mayor Edad", "#"*15 )
+#El codigo anterior es para separar las respuestas y dar mayor orden
 ismayorEdad = edad >= 18
 print (ismayorEdad)
+#Para generar la pregunta se debe digitar is al principio del codigo
 print ("#"*15, "Bajo la Estatura promedio", "#"*15)
 isMayorEstatura = estatura < 1.70 
 print (isMayorEstatura) 
